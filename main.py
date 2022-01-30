415086646e22181dde0023e20d0a7d8748eb626d
Jennifer Krieger <mail@jenniferkrieger.de>
2022-01-30 16:02:43 +0100

    [lsl] ✨ add stripping of sources, javadocs and tests

--- main.py
+++ main.py
@@ -12,21 +12,21 @@ JsonDict = Dict[str, Any]
 
 def load_json(filename: str) -> list[JsonDict]:
     """Load a dumped json file"""
-    logging.debug(f"(load_json) Attempting to read data from {filename}")
+    logging.debug(f"Attempting to read data from {filename}")
     with open(filename) as f:
         data = json.load(f)
-        logging.debug(f"(load_json) Loaded {len(data)} entries")
+        logging.debug(f"Loaded {len(data)} entries")
         return data
 
 
 def save_json(data: list[Dict[str, Any]], filename: str):
     """Save data to a given path"""
-    logging.debug(f"(save_json) Attempting to save data to {filename}")
+    logging.debug(f"Attempting to save data to {filename}")
     with open(filename, "w") as f:
         json.dump(data, f, indent=4)
 
 
-def main(filename: str, type: str, plot: bool, process: bool, output_path=""):
+def main(filename: str, type: str, plot: bool, strip: bool, output_path=""):
     # get a json
     if type == "lsl":
         data = process_lsl.lsl_to_json(filename)
@@ -39,6 +39,8 @@ def main(filename: str, type: str, plot: bool, process: bool, output_path=""):
         sys.exit()
 
     # process the json
+    if strip:
+        data = process_lsl.strip_docs_sources_tests(data)
 
     # save the json
     if output_path is not None:
@@ -55,9 +57,10 @@ if __name__ == "__main__":
     parser.add_argument('type', type=str, help='mdg or lsl')
     parser.add_argument('--process', action='store_true', help='If given, process a file')
     parser.add_argument('--plot', action='store_true', help='If given, plot things')
+    parser.add_argument('--strip', action='store_true', help='If given, cleanup lsl file')
     parser.add_argument('-o', '--output', metavar='path', action='store', help='Path to save results to')
     args = parser.parse_args(sys.argv[1:])
 
     # let's go
     logging.debug(f"Trying to process {args.input}")
-    main(args.input, type=args.type, plot=args.plot, process=args.process, output_path=args.output)
+    main(args.input, type=args.type, plot=args.plot, strip=args.strip, output_path=args.output)
