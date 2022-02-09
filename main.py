e7e18b1b9860a55950cf58fa13575b5650b860fe
Jennifer Krieger <mail@jenniferkrieger.de>
2022-02-09 11:51:48 +0100

    [] 🗑️ ♻️ major refactoring removing all json-code to prepare for postgres-migration

--- main.py
+++ main.py
@@ -5,27 +5,17 @@ import sys
 import analyze_database
 import jsonutils
 import process_lsl
-import process_mdg
 
 
-def main(filename: str, type: str, shrink: bool, strip: bool,
+def main(filename: str, type: str, shrink: bool,
          database_path="", output_path=""):
-    # get a json from lsl file
-    if type == "lsl":
-        data = process_lsl.lsl_to_json(filename)
-        # process the json
-        if strip:
-            data = process_lsl.strip_docs_sources_tests(data)
-        # save the json
-        if output_path is not None:
-            jsonutils.save_json(data, output_path)
     # load lsl to sqlite database and build index
-    elif type == "lsl-db":
+    if type == "lsl-db":
         process_lsl.lsl_to_database(filename, database_path, shrink)
         process_lsl.build_indices(database_path)
     # get a json from mdg file
     elif type == "mdg":
-        data = process_mdg.mdg_to_json(filename)
+        pass
     # load a previously stored json file
     elif type == "json":
         data = jsonutils.load_json(filename)
@@ -47,12 +37,11 @@ if __name__ == "__main__":
     parser.add_argument('type', type=str, help='mdg or lsl')
     parser.add_argument('--process', action='store_true', help='If given, process a file')
     parser.add_argument('--shrink', action='store_true', help='If given, load only j-type and drop all others')
-    parser.add_argument('--strip', action='store_true', help='If given, cleanup lsl file')
     parser.add_argument('-db', '--database', metavar='path', action='store', help='Filename of database')
     parser.add_argument('-o', '--output', metavar='path', action='store', help='Path to save results to')
     args = parser.parse_args(sys.argv[1:])
 
     # let's go
     logging.debug(f"Trying to process {args.input}")
-    main(args.input, type=args.type, shrink=args.shrink, strip=args.strip,
+    main(args.input, type=args.type, shrink=args.shrink,
          database_path=args.database, output_path=args.output)
--- process_lsl.py
+++ process_lsl.py
@@ -4,83 +4,13 @@ Tool for parsing the output of rclones (https://github.com/rclone/rclone) `lsl` 
 @date Jan. 2022
 """
 import csv
-import datetime
 import logging
 import sqlite3
-from typing import Dict, Any
 
 from dateutil import parser
 
 # Types
 from utils import determine_versionscheme_raemaekers
-
-JsonDict = Dict[str, Any]
-
-
-def lsl_to_json(filename: str) -> list[JsonDict]:
-    """Read a lsl-file and convert it to json"""
-    result: list = []
-    try:
-        with open(filename) as lsl_file:
-            reader = csv.DictReader(lsl_file, delimiter=' ', fieldnames=['size', 'date', 'time', 'path'],
-                                    skipinitialspace=True)
-
-            count = 0
-            for line in reader:
-                try:
-                    # stitch together timestamp
-                    timestamp: datetime = parser.parse(f"{line['date']} {line['time']}").isoformat()
-
-                    # split the path
-                    groupid, artifactid, version, jarname = line['path'].rsplit('/', 3)
-
-                    # convert groupid
-                    groupid_clean = groupid.replace('/', '.')
-
-                    temp_dict = dict(groupid=groupid_clean,
-                                     artifactid=artifactid,
-                                     version=version,
-                                     jarname=jarname,
-                                     timestamp=timestamp)
-                    result.append(temp_dict)
-                    count += 1
-                    if count % 1000000 == 0:
-                        logging.debug(f"Lines processed: {count}")
-                except ValueError as err:
-                    logging.error(f"ValueError: {err}\n {line}")
-            logging.info(f"Done! Lines processed: {count}")
-    except IOError:
-        logging.critical(f"File {filename} not readable!")
-    return result
-
-
-def strip_docs_sources_tests(artifact_list: list[JsonDict]) -> list[JsonDict]:
-    """Remove entries from a list of artifacts that contain -javadoc, -sources or -tests"""
-    logging.debug(f"Try to cleanup a list of {len(artifact_list)} jars")
-    result_list: list[JsonDict] = []
-
-    # filter javadocs, sources and tests
-    javadoc_count = 0
-    sources_count = 0
-    tests_count = 0
-    for artifact in artifact_list:
-        try:
-            critical_part = artifact['jarname'][-10:-4]
-            if critical_part == "avadoc":
-                javadoc_count += 1
-            elif critical_part == "ources":
-                sources_count += 1
-            elif critical_part == "-tests":
-                tests_count += 1
-            else:
-                result_list.append(artifact)
-        except ValueError as err:
-            logging.critical(f"ValueError: {err}\n {artifact}")
-    logging.info(f"Stripped {javadoc_count} javadoc.jars")
-    logging.info(f"Stripped {sources_count} sources.jars")
-    logging.info(f"Stripped {tests_count} -tests.jars")
-    logging.info(f"Remaining jars: {len(result_list)}")
-    return result_list
 
 
 def lsl_to_database(filename: str, database_file: str, shrink: bool):
